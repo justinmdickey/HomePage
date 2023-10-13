e71256faf1f85ec6a808c6afa04ff86bd9c64ad5
justinmdickey <justin.dickey.eg@gmail.com>
2023-10-13 12:22:51 -0400

    Create weather.js

---
+++ weather.js
@@ -0,0 +1,25 @@
+// weather.js
+const apiKey = 'your-api-key-here';
+const city = 'your-city-here';
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+async function getWeather() {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error('Network response was not ok ' + response.statusText);
+    const weatherData = await response.json();
+    displayWeather(weatherData);
+  } catch (error) {
+    console.error('There has been a problem with your fetch operation:', error);
+  }
+}
+
+function displayWeather(data) {
+  const temp = data.main.temp;
+  const conditions = data.weather[0].description;
+  document.getElementById('temperature').innerText = `Temperature: ${temp}°F`;
+  document.getElementById('conditions').innerText = `Conditions: ${conditions}`;
+}
+
+// Call the function to get and display the weather
+getWeather();
